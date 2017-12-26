d506c948a59fcf2e28b954b7e7de7a6b32e332b4
xSakix <seckarovci@gmail.com>
2017-12-26 12:26:11 +0100

    - added stock method for picking stocks based on Birch clustering of stocks which would satisfy rebalancing method of investment(in progress)

--- simulate_sp500.py
+++ simulate_sp500.py
@@ -4,7 +4,7 @@ from pandas_datareader._utils import RemoteDataError
 import rebalancer
 
 
-def interpet_results(assets, rebalance_inv, bah_inv, data):
+def interpet_results(assets, rebalance_inv, bah_inv, data, condition, dir):
     prices = []
     for key in data.keys():
         prices.append(data[key][data.index[-1]])
@@ -12,18 +12,18 @@ def interpet_results(assets, rebalance_inv, bah_inv, data):
     rebalancer.writeResults('REBALANCE:', data, prices, rebalance_inv)
     rebalancer.writeResults('B&H:', data, prices, bah_inv)
 
-    if rebalance_inv.history[-1] > bah_inv.history[-1]:
+    if condition:
         for key in data.keys():
-            plt.plot(data[key])
+            plt.plot(data[key], color='black')
         plt.axis('off')
-        plt.savefig('stock_results/' + assets[0] + '_' + assets[1] + '.png')
+        plt.savefig(dir + assets[0] + '_' + assets[1] + '.png')
         plt.clf()
 
 
 with open('s&p500.txt', 'r') as fd:
     stocks = list(fd.read().splitlines())
 
-stocks = stocks[:20]
+stocks = stocks[:10]
 
 start_date = '2010-01-01'
 end_date = '2017-12-12'
@@ -41,6 +41,10 @@ for stock in stock_list:
     try:
         data, data2 = rebalancer.load_data(stock, end_date, start_date)
         rebalance_inv, bah_inv = rebalancer.simulate(data, data2, crypto=False)
-        interpet_results(stock, rebalance_inv, bah_inv, data)
+
+        # condition = rebalance_inv.history[-1] > bah_inv.history[-1]
+        # interpet_results(stock, rebalance_inv, bah_inv, data,condition,'stock_results/')
+        condition2 = rebalance_inv.history[-1] < bah_inv.history[-1]
+        interpet_results(stock, rebalance_inv, bah_inv, data, condition2, 'stock_bah_results/')
     except RemoteDataError or IndexError:
         print('error simulating:' + str(stock))
--- stock_method.py
+++ stock_method.py
@@ -1,42 +1,80 @@
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
-from sklearn.cluster import KMeans
-
+from sklearn.cluster import KMeans, DBSCAN, MeanShift, AffinityPropagation, Birch
+from sklearn.mixture import GaussianMixture
 import numpy as np
 import os
+from sklearn.decomposition import PCA
 
-data_dir = 'stock_results'
 
-imgs = []
-names = []
-files = os.listdir(data_dir)
-files = files[:40]
-for file in files:
-    print('loading:' + file)
-    img = mpimg.imread(data_dir + '/' + file)
-    names.append(file)
-    imgs.append(img)
+def load_data(data_dir):
+    imgs = []
+    names = []
+    files = os.listdir(data_dir)
+    # files = files[:10]
+    for file in files:
+        print('loading:' + file)
+        img = mpimg.imread(data_dir + '/' + file)
+        # img = Image.open(data_dir + '/' + file).convert('1')
+        # img.save(data_dir + '/' + file)
+        names.append(file)
+        imgs.append(np.asarray(img))
 
+    return names, imgs
+
+
+def find_clusters(data, imgs, names):
+    pca = PCA(n_components=len(imgs))
+    data = pca.fit(data).transform(data)
+    # kmeans = KMeans(init='k-means++', n_clusters=2, n_init=3).fit(data)
+    # labels = kmeans.labels_
+    #
+    # labels = kmeans.predict(data).tolist()
+    # db = DBSCAN().fit(data)
+    # labels = db.labels_
+    # shift = MeanShift().fit(data)
+    # labels = shift.labels_
+    # affinity = AffinityPropagation().fit(data)
+    # labels = affinity.labels_
+    # gaussian = GaussianMixture().fit(data)
+    # labels = gaussian.predict(data)
+    birch = Birch().fit(data)
+    labels = birch.labels_
+    print(labels)
+    predictions = list(zip(names, labels))
+
+    return predictions
+
+
+def interpret_results(predictions, imgs):
+    classes = []
+    for (name, label) in predictions:
+        if not classes.__contains__(label):
+            classes.append(label)
+
+    print('classes: ' + str(classes))
+
+    num = 2
+    counter = np.zeros(len(classes))
+
+    index = 0
+    for (name, label) in predictions:
+        print('prediction =>' + str(name) + '->' + str(label))
+        if (counter[int(label)] == num):
+            continue
+        counter[int(label)] += 1
+        plt.subplot(len(classes), num, index + 1)
+        plt.axis('off')
+        plt.imshow(imgs[index], interpolation='nearest')
+        plt.title(str(name) + '->' + str(label))
+        index += 1
+    plt.show()
+
+
+names, imgs = load_data('train_stock')
 
 data = np.array(imgs).reshape((len(imgs), -1))
 
+predictions = find_clusters(data, imgs, names)
 
-kmeans = KMeans(init='k-means++', n_clusters=len(imgs), n_init=3)
-kmeans.fit(data)
-
-labels = kmeans.predict(data).tolist()
-
-predictions = list(zip(names, labels))
-
-num = int(len(imgs) / 4)+1
-print(num)
-
-index = 0
-for index, (name, label) in enumerate(predictions):
-    print('prediction =>' + str(name) + '->' + str(label))
-    plt.subplot(num, 4, index + 1)
-    plt.axis('off')
-    plt.imshow(imgs[index], interpolation='nearest')
-    plt.title(str(name) + '->' + str(label))
-
-plt.show()
+interpret_results(predictions, imgs)
